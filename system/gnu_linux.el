d025fab99ea603d9e63bc2bac6e76cc8202a3a3f
kagenel <seizaonefist@gmail.com>
2018-11-19 13:18:35 +0900

    update

--- system/gnu_linux.el
+++ system/gnu_linux.el
@@ -5,3 +5,14 @@
 ;; (setq default-input-method "japanese-mozc")
 
 ;; ;; (global-set-key (kbd "C-j") 'mozc-mode)
+
+
+
+(require 'mozc)
+;; 日本語入力を japanese-mozc に
+(setq default-input-method "japanese-mozc")
+;; 日本語入力切り替えのキーを定義
+(global-set-key (kbd "C-<space>") 'toggle-input-method)
+
+
+
