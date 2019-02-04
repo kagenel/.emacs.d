23cd2793163436ac8f40afb74c2dfc23c4cb6d12
kagenel <seizaonefist@gmail.com>
2019-02-04 10:54:48 +0900

    add alpha, shell-pop

--- init.el
+++ init.el
@@ -10,6 +10,13 @@
 ;;        └ x11.el         ; 共通の設定(GUI)
 
 ;; 脆弱性対策
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (eval-after-load "enriched"
     '(defun enriched-decode-display-prop (start end &optional param)
        (list start end)))
@@ -36,3 +43,34 @@
   (load-file "~/.emacs.d/window/x11.el"))
 ))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(init-loader-show-log-after-init (quote error-only))
+ '(package-selected-packages
+   (quote
+    (yatex yaml-mode undo-tree tabbar swap-buffers popwin perspective neotree init-loader google-translate flycheck ac-math)))
+ '(safe-local-variable-values
+   (quote
+    ((mode . japanese-latex)
+     (TeX-master . t)
+     (TeX-master . main))))
+ '(shell-pop-default-directory "/Users/kyagi/git")
+ '(shell-pop-full-span t)
+ '(shell-pop-shell-type
+   (quote
+    ("ansi-term" "*ansi-term*"
+     (lambda nil
+       (ansi-term shell-pop-term-shell)))))
+ '(shell-pop-term-shell "/bin/bash")
+ '(shell-pop-universal-key "C-t")
+ '(shell-pop-window-position "right")
+ '(shell-pop-window-size 30))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
